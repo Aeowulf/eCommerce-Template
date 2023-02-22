13c12877aa8baf26c726e44754a31a75a32a1094
Alex Wulf <alexaethelwulf@gmail.com>
2023-02-21 23:19:28 -0600

    Added "removeFromCart" functionality to CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link, useParams, useNavigate, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = () => {
   const { productId } = useParams();
@@ -26,7 +26,7 @@ const CartScreen = () => {
   }, [dispatch, productId, qty])
 
   const removeFromCartHandler = (id) => {
-    console.log('Removed')
+    dispatch(removeFromCart(id))
   }
 
   const checkoutHandler = () => {
