d6569b436bbb88486ecfd5c4a595ab4d8be29037
Alex Wulf <alexaethelwulf@gmail.com>
2023-02-22 20:19:15 -0600

    Created web token authorization 4 protected routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,10 +30,30 @@ const authUser = asyncHandler(async (req, res) => {
     })
   } else/* if user is not found or password doesn't match*/{
       res.status(401)
-      // Return unauthorized error
+      // Return "Unauthorized" error
       throw new Error('Invalid email or password')
       // Throw error message
   }
 })
 
-export { authUser }
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+      res.json({
+        _id:      user._id    ,
+        name:     user.name   ,
+        email:    user.email  ,
+        isAdmin:  user.isAdmin,
+      })
+  } else {
+      res.status(404)
+      throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 // Copied productRoutes.js to get started
 
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
 // Posts authUser data to the login page
+
+router.route('/profile').get(protect, getUserProfile)
+// Added protective middleware to Profile route
 
 export default router
