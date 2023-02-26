1d5c2decd312ba1de9378bbe783f3b985db3d1be
Alex Wulf <alexaethelwulf@gmail.com>
2023-02-26 16:48:24 -0600

    Created updateOrderToPaid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { 
   addOrderItems,
-  getOrderById
+  getOrderById,
+  updateOrderToPaid
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -10,5 +11,7 @@ const router = express.Router()
 router.route('/').post(protect, addOrderItems)
 
 router.route('/:id').get(protect, getOrderById)
+
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 export default router
