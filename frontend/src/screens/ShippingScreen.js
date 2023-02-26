7d92818ba7796d97f8bca37eddd38d6c99a2e479
Alex Wulf <alexaethelwulf@gmail.com>
2023-02-25 22:55:04 -0600

    Created Checkout Steps Component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = () => {
@@ -28,6 +29,8 @@ const ShippingScreen = () => {
   }
 
   return <FormContainer>
+    <CheckoutSteps step1 step2 />
+
     <h1>Shipping</h1>
 
     <Form onSubmit={submitHandler}>
