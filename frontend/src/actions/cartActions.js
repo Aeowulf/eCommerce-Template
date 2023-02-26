1eecaad6d2970535ee89fa628390e67e9e08cbc9
Alex Wulf <alexaethelwulf@gmail.com>
2023-02-25 21:43:14 -0600

    Created Shipping Screen&Save Address functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { 
+  CART_ADD_ITEM, 
+  CART_REMOVE_ITEM, 
+  CART_SAVE_SHIPPING_ADDRESS 
+} from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
@@ -31,3 +35,12 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
   // Updates localStorage State now that dispatch has removed targeted item from cartItems
 }
+
+export const saveShippingAddress = (data) => (dispatch) => {  
+    dispatch({
+      type: CART_SAVE_SHIPPING_ADDRESS,
+      payload: data
+    })
+  
+    localStorage.setItem('shippingAddress', JSON.stringify(data))
+  }
