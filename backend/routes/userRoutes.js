c2d160f3f95e882bfad0223a0854d926a62f8d54
Alex Wulf <alexaethelwulf@gmail.com>
2023-02-28 02:31:37 -0600

    Added getUserById & updateUser Endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -23,6 +25,10 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
 
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
