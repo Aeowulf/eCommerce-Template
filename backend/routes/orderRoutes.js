fba9003719c7ad1631efb935f0f5a0675c6bb987
Alex Wulf <alexaethelwulf@gmail.com>
2023-03-01 23:56:49 -0600

    Added Mark Order As Delivered Functionality

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders,
 } from '../controllers/orderController.js'
@@ -17,5 +18,7 @@ router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+
+router.route('/:id/deliver').put(protect, updateOrderToDelivered)
 
 export default router
