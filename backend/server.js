6dd0fba0d4df297b6427f9da0b71845e6545e2a8
Alex Wulf <alexaethelwulf@gmail.com>
2023-03-02 20:08:34 -0600

    InstalledMorgan & addedcreateProductReviewEndpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -19,7 +20,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-// Allows us to accept JSON data in the body
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.get('/', (req, res) => {
   res.send('API is running...')
